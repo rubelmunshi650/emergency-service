d395feb981038d4dec9e0c3fb3a1892d8aec1f2d
rubelmunshi650 <rubelmunshi650@gmail.com>
2025-08-28 16:53:14 +0600

    Added service number copy and copy count js

--- js/script.js
+++ js/script.js
@@ -57,3 +57,155 @@ document.getElementById('like-9').addEventListener('click', function () {
 });
 
 // ----- like and like count js end --------
+
+
+// ------------ number copy and copy count js start ----------------
+
+let copyCount = parseInt(document.getElementById('copy-count').innerText);
+
+document.getElementById('copy-btn-1').addEventListener('click', function () {
+    const copyArea = document.getElementById('copy-text-1').innerText;
+    navigator.clipboard.writeText(copyArea)
+        .then(() => {
+            alert('Copied this service number : ' + copyArea);
+        })
+        .catch(err => {
+            alert('❌ Failed to copy!');
+            console.error(err);
+        });
+    copyCount++;
+    document.getElementById('copy-count').innerText = copyCount;
+
+
+});
+
+document.getElementById('copy-btn-2').addEventListener('click', function () {
+    const copyArea = document.getElementById('copy-text-2').innerText;
+    navigator.clipboard.writeText(copyArea)
+        .then(() => {
+            alert('Copied this service number : ' + copyArea);
+        })
+        .catch(err => {
+            alert('❌ Failed to copy!');
+            console.error(err);
+        });
+    copyCount++;
+    document.getElementById('copy-count').innerText = copyCount;
+
+
+});
+
+document.getElementById('copy-btn-3').addEventListener('click', function () {
+    const copyArea = document.getElementById('copy-text-3').innerText;
+    navigator.clipboard.writeText(copyArea)
+        .then(() => {
+            alert('Copied this service number : ' + copyArea);
+        })
+        .catch(err => {
+            alert('❌ Failed to copy!');
+            console.error(err);
+        });
+    copyCount++;
+    document.getElementById('copy-count').innerText = copyCount;
+
+
+});
+
+document.getElementById('copy-btn-4').addEventListener('click', function () {
+    const copyArea = document.getElementById('copy-text-4').innerText;
+    navigator.clipboard.writeText(copyArea)
+        .then(() => {
+            alert('Copied this service number : ' + copyArea);
+        })
+        .catch(err => {
+            alert('❌ Failed to copy!');
+            console.error(err);
+        });
+    copyCount++;
+    document.getElementById('copy-count').innerText = copyCount;
+
+
+});
+
+document.getElementById('copy-btn-5').addEventListener('click', function () {
+    const copyArea = document.getElementById('copy-text-5').innerText;
+    navigator.clipboard.writeText(copyArea)
+        .then(() => {
+            alert('Copied this service number : ' + copyArea);
+        })
+        .catch(err => {
+            alert('❌ Failed to copy!');
+            console.error(err);
+        });
+    copyCount++;
+    document.getElementById('copy-count').innerText = copyCount;
+
+
+});
+
+document.getElementById('copy-btn-6').addEventListener('click', function () {
+    const copyArea = document.getElementById('copy-text-6').innerText;
+    navigator.clipboard.writeText(copyArea)
+        .then(() => {
+            alert('Copied this service number : ' + copyArea);
+        })
+        .catch(err => {
+            alert('❌ Failed to copy!');
+            console.error(err);
+        });
+    copyCount++;
+    document.getElementById('copy-count').innerText = copyCount;
+
+
+});
+
+document.getElementById('copy-btn-7').addEventListener('click', function () {
+    const copyArea = document.getElementById('copy-text-7').innerText;
+    navigator.clipboard.writeText(copyArea)
+        .then(() => {
+            alert('Copied this service number : ' + copyArea);
+        })
+        .catch(err => {
+            alert('❌ Failed to copy!');
+            console.error(err);
+        });
+    copyCount++;
+    document.getElementById('copy-count').innerText = copyCount;
+
+
+});
+
+document.getElementById('copy-btn-8').addEventListener('click', function () {
+    const copyArea = document.getElementById('copy-text-8').innerText;
+    navigator.clipboard.writeText(copyArea)
+        .then(() => {
+            alert('Copied this service number : ' + copyArea);
+        })
+        .catch(err => {
+            alert('❌ Failed to copy!');
+            console.error(err);
+        });
+    copyCount++;
+    document.getElementById('copy-count').innerText = copyCount;
+
+
+});
+
+document.getElementById('copy-btn-9').addEventListener('click', function () {
+    const copyArea = document.getElementById('copy-text-9').innerText;
+    navigator.clipboard.writeText(copyArea)
+        .then(() => {
+            alert('Copied this service number : ' + copyArea);
+        })
+        .catch(err => {
+            alert('❌ Failed to copy!');
+            console.error(err);
+        });
+    copyCount++;
+    document.getElementById('copy-count').innerText = copyCount;
+
+
+});
+
+
+// ------------ number copy and copy count js end ----------------
