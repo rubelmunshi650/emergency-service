d3f7fc6a97d31a916bc4dbe1ebe90e3ad500d743
rubelmunshi650 <rubelmunshi650@gmail.com>
2025-08-28 16:50:16 +0600

    Added card like and like count js

---
+++ js/script.js
@@ -0,0 +1,59 @@
+// ----- like and like count js start --------
+let likeCount = parseInt(document.getElementById('like-count').innerText);
+
+document.getElementById('like-1').addEventListener('click', function () {
+    likeCount++;
+    document.getElementById('like-count').innerText = likeCount;
+    const color = this.classList.add('text-red-600');
+});
+
+document.getElementById('like-2').addEventListener('click', function () {
+    likeCount++;
+    document.getElementById('like-count').innerText = likeCount;
+    const color = this.classList.add('text-red-600');
+});
+
+document.getElementById('like-3').addEventListener('click', function () {
+    likeCount++;
+    document.getElementById('like-count').innerText = likeCount;
+    const color = this.classList.add('text-red-600');
+});
+
+document.getElementById('like-4').addEventListener('click', function () {
+    likeCount++;
+    document.getElementById('like-count').innerText = likeCount;
+    const color = this.classList.add('text-red-600');
+});
+
+document.getElementById('like-5').addEventListener('click', function () {
+    likeCount++;
+    document.getElementById('like-count').innerText = likeCount;
+    const color = this.classList.add('text-red-600');
+});
+
+document.getElementById('like-6').addEventListener('click', function () {
+    likeCount++;
+    document.getElementById('like-count').innerText = likeCount;
+    const color = this.classList.add('text-red-600');
+});
+
+document.getElementById('like-7').addEventListener('click', function () {
+    likeCount++;
+    document.getElementById('like-count').innerText = likeCount;
+    const color = this.classList.add('text-red-600');
+});
+
+document.getElementById('like-8').addEventListener('click', function () {
+    likeCount++;
+    document.getElementById('like-count').innerText = likeCount;
+    const color = this.classList.add('text-red-600');
+});
+
+document.getElementById('like-9').addEventListener('click', function () {
+    likeCount++;
+    document.getElementById('like-count').innerText = likeCount;
+    const color = this.classList.add('text-red-600');
+
+});
+
+// ----- like and like count js end --------
